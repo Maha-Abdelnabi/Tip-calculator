d438e610a88e143300441bf39db01eed374f383e
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-05 00:27:25 -0400

    calculate the total tip

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const btnEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const totalIEl = document.getElementById("total");
 
 
 function calculatTotal(){
-   const billValue = billInput.value;
-   console.log (billValue)
+  const billValue = billInput.value;
+  console.log(billValue);
 
-   const tipValue = tipInput.value;
-   console.log(tipValue);
-   
+  const tipValue = tipInput.value;
+  console.log(tipValue);
+  //calculate tip multiply the total check by 1 plus the decimal percentage
+  const totalTip =Math.floor(billValue * (1 + tipValue / 100));
+  totalIEl.innerText = `${totalTip}`
 }
 
 btnEl.addEventListener("click", calculatTotal)
