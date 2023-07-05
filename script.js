da5eaf7f2e64166edffcb045c817854a21ca17a1
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-05 00:43:30 -0400

    add person input

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const btnEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const personInput = document.getElementById("person");
 const totalIEl = document.getElementById("total");
+const shartEl = document.getElementById("share");
 
 
 function calculatTotal(){
   const billValue = billInput.value;
-  console.log(billValue);
+
 
   const tipValue = tipInput.value;
-  console.log(tipValue);
+
   //calculate tip multiply the total check by 1 plus the decimal percentage
   const totalTip =Math.floor(billValue * (1 + tipValue / 100));
-  totalIEl.innerText = `${totalTip}`
+
+
+//divide the bill 
+const personValue = personInput.value;
+
+const sharedBill = totalTip/personValue;
+
+
+if(personValue > 1){
+    shartEl.innerHTML=`Each Person should pay: ${sharedBill}`;
+}
+else{
+   shartEl.innerHTML=`You should pay the total amount of: ${totalTip}` 
+}
 }
 
 btnEl.addEventListener("click", calculatTotal)
