5d081efc30397cf8c80a2f8fe11a6a2484f50d88
Maha Abdelnabi <mahaomar@gmail.com>
2023-07-04 23:51:10 -0400

    console log the the vill and tip value

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const btnEl = document.getElementById("calculate");
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
 
 
 function calculatTotal(){
-    console.log ("clicked")
+   const billValue = billInput.value;
+   console.log (billValue)
+
+   const tipValue = tipInput.value;
+   console.log(tipValue);
+   
 }
 
 btnEl.addEventListener("click", calculatTotal)
